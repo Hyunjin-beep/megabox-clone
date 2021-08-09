2b7608504352222610d7af6338243000b9f8dc39
Hyunjin Cho <jhj176610@gmail.com>
2021-08-10 02:46:01 +0900

    Style for movie-ads: header and starting for slider

---
+++ src/main.js
@@ -0,0 +1,7 @@
+//movie-service-slideAds
+const prev = document.querySelector(".moveToLeft");
+const next = document.querySelector(".moveToRight");
+const adsContainer = document.querySelector(".movie-service-slideAds-wrapper");
+console.log(adsContainer);
+// const totalAds = ads.length;
+// let index = 0;
