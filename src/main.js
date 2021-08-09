a7fd6c410a93e0942b3b3eaf9a4e6444a6710c9d
Hyunjin Cho <jhj176610@gmail.com>
2021-08-10 03:09:39 +0900

    style and js for movie-ads: advertisement images slides

--- src/main.js
+++ src/main.js
@@ -1,7 +1,44 @@
+"use strict";
+
 //movie-service-slideAds
 const prev = document.querySelector(".moveToLeft");
 const next = document.querySelector(".moveToRight");
-const adsContainer = document.querySelector(".movie-service-slideAds-wrapper");
-console.log(adsContainer);
-// const totalAds = ads.length;
-// let index = 0;
+const ads = document.querySelector(".movie-service-slideAds-wrapper").children;
+const caption = document.querySelector(".caption");
+const totalAds = ads.length;
+let index = 0;
+console.log(totalAds);
+
+caption.innerText = `1/ 9`;
+
+prev.addEventListener("click", () => {
+  nextImage("prev");
+});
+
+next.addEventListener("click", () => {
+  nextImage("next");
+});
+
+function nextImage(direction) {
+  if (direction == "next") {
+    index++;
+    if (index == totalAds) {
+      index = 0;
+    }
+  } else {
+    if (index == 0) {
+      index = totalAds - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for (let i = 0; i < ads.length; i++) {
+    ads[i].classList.remove("main");
+    ads[i].classList.add("others");
+    caption.innerText = `${i + 1}/ 9`;
+  }
+  ads[index].classList.add("main");
+  ads[index].classList.remove("others");
+  caption.innerText = `${index + 1}/ 9`;
+}
