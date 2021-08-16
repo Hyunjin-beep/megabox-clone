73a540694c32ef11959ed06566020c66ed0499fa
Hyunjin Cho <jhj176610@gmail.com>
2021-08-16 13:38:51 -0500

    js for automatic image slide show

--- src/main.js
+++ src/main.js
@@ -7,12 +7,10 @@ const ads = document.querySelector(".movie-service-slideAds-wrapper").children;
 const caption = document.querySelector(".caption");
 const totalAds = ads.length;
 let index = 0;
-console.log(totalAds);
 
 const captionBar = document.querySelector(".caption-slide").children;
 const totalCaptionBar = captionBar.length;
 let captionBarIndex = 0;
-console.log(totalCaptionBar);
 
 caption.innerText = `1/ 9`;
 
@@ -39,19 +37,66 @@ function nextImage(direction) {
   }
 
   for (let i = 0; i < ads.length; i++) {
+    // present img
     ads[i].classList.remove("main");
     ads[i].classList.add("others");
 
-    captionBar[i].classList.remove("caption-main");
-    captionBar[i].classList.add("caption-others");
+    // upcoming img
+    ads[index].classList.add("main");
+    ads[index].classList.remove("others");
 
-    caption.innerText = `${i + 1}/ 9`;
+    caption.innerText = `${index + 1}/ 9`;
   }
-  ads[index].classList.add("main");
-  ads[index].classList.remove("others");
-
-  captionBar[captionBarIndex].classList.add("caption-main");
-  captionBar[captionBarIndex].classList.remove("caption-others");
-
-  caption.innerText = `${index + 1}/ 9`;
 }
+
+const play = document.querySelector(".playAdsSlide");
+let playStatus = false;
+
+play.addEventListener("click", () => {
+  playImgAutomatically();
+});
+
+function playImgAutomatically() {
+  let imgTest;
+  if (playStatus === false) {
+    play.innerHTML = '<i class="fas fa-pause"></i>';
+    playStatus = true;
+
+    imgTest = setInterval(function () {
+      nextImage("next");
+    }, 4000);
+  } else {
+    play.innerHTML = '<i class="fas fa-caret-right"></i>';
+    playStatus = false;
+    console.log("stop");
+    clearInterval(imgTest);
+  }
+}
+
+// function startImgTest() {
+//   console.log("start");
+//   myCallBackTwo("next");
+// }
+
+// function stopImgTest() {
+//   console.log("stop");
+//   clearInterval(imgTest);
+// }
+
+// function myCallT(t) {
+//   console.log(t);
+// }
+
+// let setTest;
+
+// function startTest() {
+//   setTest = setInterval(myCallBack, 1000);
+// }
+
+// function stopTest() {
+//   clearInterval(setTest);
+// }
+
+// function myCallBack() {
+//   console.log("div");
+// }
